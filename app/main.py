a705994ccd189680ec264364f21ba1c4b1c85d0e
Alice Gray <94911337+alicegray33@users.noreply.github.com>
2022-02-22 14:35:04 +0000

    Updated app to learn how to deploy

--- app/main.py
+++ app/main.py
@@ -2,4 +2,4 @@ from flask import Flask
 app= Flask(__name__)
 @app.route('/')
 def index():
-  return "<h1>Hello Cloud World!</h1><br><br>Alice"
+  return "<h1>Hello Cloud World!</h1><br><br>A Flask app created by Alice/Team 3"
